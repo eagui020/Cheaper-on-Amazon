5973e5288e0c2822a5de84688f26a4d66771b504
Eric Aguirre <eagui020@ucr.edu>
2018-10-25 19:22:54 -0700

    Added script injector

---
+++ src/CheaperOnChrome/app/src/browser_action/popup.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function () {
+    $("#options").click(function () {
+        chrome.runtime.openOptionsPage();
+    });
+    
+    var status = "";
+    var onoff;
+    chrome.storage.sync.get('status', function (result) {
+        status = result.status;
+        if (status == null || status == "on") {
+            $("#onoff").prop('checked', true);
+            onoff = true;
+            chrome.browserAction.setIcon({ path: "../../img/get_started32.png" });
+        }
+        else
+        {
+            $("#onoff").prop('checked', false);
+            onoff = false;
+            chrome.browserAction.setIcon({ path: "../../img/off.png" });
+        }
+    }); 
+    
+    $(".slider").click(function (tab) {
+        if ($("#onoff").prop('checked')) {
+            onoff = false;
+        }
+        else {
+            onoff = true;
+        }
+        if (!onoff) {
+            chrome.storage.sync.set({ 'status': "off" });
+            chrome.browserAction.setIcon({ path: "../../img/off.png", tabId: tab.id });
+        }
+        else {
+            chrome.storage.sync.set({ 'status': "on" });
+            chrome.browserAction.setIcon({ path: "../../img/get_started32.png", tabId: tab.id });
+        }
+    });
+
+    $("#history").click(function () {
+        window.open('../history/history.html', '_blank');
+    });
+});
