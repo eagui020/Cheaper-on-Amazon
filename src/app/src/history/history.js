5617ab9720c9263462b316f3de930a403fc758b1
kmo004 <kmo004@ucr.edu>
2018-10-26 12:48:48 -0700

    Added Recent History module

---
+++ src/app/src/history/history.js
@@ -0,0 +1,23 @@
+chrome.storage.sync.get(null, function (result) {
+	for(var i = 0; i < result.histUrl.length; ++i)
+	{
+		// Get all history
+		var histUrl = result.histUrl[i];
+		var histName = result.histName[i];
+
+		// Dynamically create search history list
+		var div = document.createElement('div');
+		div.className = "card w-50 mx-auto";
+		div.innerHTML = "<div class='card-body'> <a href='" + histUrl + "' style='display: block; text-align: center; text-decoration: none;'>" + histName + "</a> </div>"
+		document.body.appendChild(div);
+	}
+}); 
+
+// Clear history
+var clear = document.getElementById("clearhistory");
+
+clear.onclick = function () { 
+	chrome.storage.sync.set({ 'histUrl': [] });
+	chrome.storage.sync.set({ 'histName': [] });
+	location.reload();
+};
