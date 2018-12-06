1cae2cca9d59d39ea6e3c1d17de31c72f9622ecc
mmira014 <marcos.am20@hotmail.com>
2018-12-05 23:22:44 -0800

    made clear buttons invisible when lists are empty

--- src/app/src/history/history.js
+++ src/app/src/history/history.js
@@ -1,44 +1,73 @@
 chrome.storage.sync.get(null, function (result) {
-	for(var i = 0; i < result.histUrl.length; ++i)
-	{
-		// Get all history
-		var histUrl = result.histUrl[i];
-		var histName = result.histName[i];
+	if(result.histUrl.length == 0) {
+		var PriceHistoryBtn = document.getElementById('testDiv');
+		PriceHistoryBtn.className="d-none";
 
-		// Dynamically create search history list
 		var div = document.createElement('div');
 		div.className = "card w-50 mx-auto";
-        div.innerHTML = "<div class='card-body'> <a href='" + histUrl + "' style='display: block; text-align: center; text-decoration: none;'>" + histName + "</a> </div>";
-		document.getElementById("pricehistory").appendChild(div);
+		div.innerHTML = "<div class='card-body'><a style='display: block; text-align: center; text-decoration: none;'>Nothing to show here.</a></div>";
+		document.getElementById("PriceHistoryAnchor").appendChild(div);
+	}
+	else {
+		var PriceHistoryBtn = document.getElementById('testDiv');
+		PriceHistoryBtn.className="card w-50 mx-auto";
+		for(var i = 0; i < result.histUrl.length; ++i)
+		{
+			// Get all history
+			var histUrl = result.histUrl[i];
+			var histName = result.histName[i];
+
+			// Dynamically create search history list
+			var div = document.createElement('div');
+			div.className = "card w-50 mx-auto";
+			div.innerHTML = "<div class='card-body'> <a href='" + histUrl + "' style='display: block; text-align: center; text-decoration: none;'>" + histName + "</a> </div>";
+			document.getElementById("pricehistory").appendChild(div);
+		}
+
 	}
 
 	// fetch search history
-	for(var i = 0; i < result.searchHistUrl.length; ++i)
-	{
-		var searchHistUrl = result.searchHistUrl[i];
-		var searchHistName = result.searchHistName[i];
+	if(result.searchHistUrl.length == 0) {
+		var SearchHistBtn = document.getElementById('test2Div');
+		SearchHistBtn.className="d-none";
 
 		var div = document.createElement('div');
 		div.className = "card w-50 mx-auto";
-		div.innerHTML = "<div class='card-body'> <a href='" + searchHistUrl + "' style='display: block; text-align: center; text-decoration: none;'>" + searchHistName + "</a> </div>"
-		document.body.appendChild(div);
+		div.innerHTML = "<div class='card-body'><a style='display: block; text-align: center; text-decoration: none;'>Nothing to show here.</a></div>";
+		document.getElementById("SearchHistoryAnchor").appendChild(div);
+	}
+	else {
+		var SearchHistBtn = document.getElementById('test2Div');
+		SearchHistBtn.className="card w-50 mx-auto";
+
+		for(var i = 0; i < result.searchHistUrl.length; ++i)
+		{
+			var searchHistUrl = result.searchHistUrl[i];
+			var searchHistName = result.searchHistName[i];
+
+			var div = document.createElement('div');
+			div.className = "card w-50 mx-auto";
+			div.innerHTML = "<div class='card-body'> <a href='" + searchHistUrl + "' style='display: block; text-align: center; text-decoration: none;'>" + searchHistName + "</a> </div>"
+			document.getElementById("searchhistory").appendChild(div);
+		}
+
 	}
 }); 
 
 // Clear history
-var clear = document.getElementById("clearhistory");
+// var clear = document.getElementById("clearhistory");
 
-clear.onclick = function () { 
+$("#clearhistory").click(function () { 
 	chrome.storage.sync.set({ 'histUrl': [] });
 	chrome.storage.sync.set({ 'histName': [] });
 	location.reload();
-};
+});
 
 // clear search history
-var clearSearch = document.getElementById("clearSearch");
+// var clearSearch = document.getElementById("clearSearch");
 
-clearSearch.onclick = function() {
+$("#clearSearch").click(function() {
 	chrome.storage.sync.set({ 'searchHistUrl': [] });
 	chrome.storage.sync.set({ 'searchHistName': [] });
 	location.reload();
-};
+});
